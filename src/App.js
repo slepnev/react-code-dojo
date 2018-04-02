db37d7f98e72629bfb2f407938e0add5797cf698
alexander <a.slepnev@it365.su>
2018-04-02 13:42:16 +0300

    CodeDjodo

--- src/App.js
+++ src/App.js
@@ -1,51 +1,34 @@
 import React, {Component} from "react";
+import logo from "./logo.svg";
+import Hero from "./Hero";
 import "./App.css";
-import Menu from "./components/Menu";
 
-let recipes = [
-  {
-    "name": "Baked Salmon",
-    "ingredients": [
-      {"name": "Salmon", "amount": 1, "measurement": "l lb"},
-      {"name": "Pine Nuts", "amount": 1, "measurement": "cup"},
-      {"name": "Butter Lettuce", "amount": 2, "measurement": "cups"},
-      {"name": "Yellow Squash", "amount": 1, "measurement": "med"},
-      {"name": "Olive Oil", "amount": 0.5, "measurement": "cup"},
-      {"name": "Garlic", "amount": 3, "measurement": "cloves"}
-    ],
-    "steps": [
-      "Preheat the oven to 350 degrees.",
-      "Spread the olive oil around a glass baking dish.",
-      "Add the salmon, garlic, and pine nuts to the dish.",
-      "Bake for 15 minutes.",
-      "Add the yellow squash and put back in the oven for 30 mins.",
-      "Remove from oven and let cool for 15 minutes. Add the lettuce and serve."
-    ]
-  },
-  {
-    "name": "Fish Tacos",
-    "ingredients": [
-      {"name": "Whitefish", "amount": 1, "measurement": "l lb"},
-      {"name": "Cheese", "amount": 1, "measurement": "cup"},
-      {"name": "Iceberg Lettuce", "amount": 2, "measurement": "cups"},
-      {"name": "Tomatoes", "amount": 2, "measurement": "large"},
-      {"name": "Tortillas", "amount": 3, "measurement": "med"}
-    ],
-    "steps": [
-      "Cook the fish on the grill until hot.",
-      "Place the fish on the 3 tortillas.",
-      "Top them with lettuce, tomatoes, and cheese."
-    ]
-  }
-];
+const list = [
+  {title: "React", subtitle: "Description on React", logo: logo},
+  {title: "Angular 2", subtitle: "Description on Angular 2", logo: "https://angular.io/assets/images/logos/angular/logo-nav@2x.png"},
+  {title: "Vue.js", subtitle: "Description on Angular 2", logo: "https://avatars.mds.yandex.net/get-entity_search/114969/250691678/S122x122FitScale"},
+]
 
 class App extends Component {
 
+  constructor(props) {
+    super();
+  }
+
+  componentDidMount() {
+    console.log("Starting");
+  }
+
+  componentWillUnmount() {
+    console.log("Update")
+  }
+
   render() {
+    console.log(list);
     return (
-      <div className="app">APP</div>
-      <Menu title="Заголовок"
-            recipes=recipes />
+      <div className="container">
+        {list.map((item, i) => <Hero logo={item.logo} title={item.title} subtitle={item.subtitle} />)}
+      </div>
     );
   }
 }
