88d051bab2385d395b1d220e7e17740fedcc246d
alexander <a.slepnev@it365.su>
2018-04-28 17:59:34 +0300

    Tooltipe

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -1,13 +1,40 @@
 import React, {Component} from "react";
+import ReactDOM from 'react-dom';
+
 
 class Tooltip extends Component {
   constructor(props) {
     super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {opacity: false};
+  }
+
+  toggle(){
+    const tooltipNode = ReactDOM.findDOMNode(this);
+    console.log(tooltipNode);
+    this.setState({
+      opacity: !this.state.opacity,
+      top: tooltipNode.offsetTop,
+      left: tooltipNode.offsetLeft
+    })
   }
 
   render() {
+    const style = {
+      zIndex: (this.state.opacity) ? 1000 : -1000,
+      opacity: +this.state.opacity,
+      top: (this.state.top || 0) + 20,
+      left: (this.state.left || 0) -30
+    };
     return (
-      Tooltip
+      <div style={{display: "inline"}}>
+        <span style={{color: "blue"}}
+          onMouseEnter={this.toggle}
+          onMouseOut={this.toggle}>
+          {this.props.children}
+        </span>
+         {this.props.text}
+      </div>
     );
   }
 }
